4db7d6b929473980f592c14b11435a428cc90470
Adam Grund <adam.grund@gmail.com>
2021-08-05 14:47:17 +0200

    PLAN-70 add svg as component

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -3,47 +3,45 @@
  * https://reactnavigation.org/docs/bottom-tab-navigator
  */
 
-import { Ionicons } from "@expo/vector-icons"
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import { createStackNavigator } from "@react-navigation/stack"
-import * as React from "react"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import MapSvg from "../assets/images/map.svg";
+import TicketSvg from "../assets/images/ticket.svg";
 
-import MapScreen from "../screens/MapScreen"
-import TabTwoScreen from "../screens/TabTwoScreen"
-import { BottomTabParamList, MapParamList, TabTwoParamList } from "../types"
+import MapScreen from "../screens/MapScreen";
+import TabTwoScreen from "../screens/TabTwoScreen";
+import { BottomTabParamList, MapParamList, TabTwoParamList } from "../types";
 
-const BottomTab = createBottomTabNavigator<BottomTabParamList>()
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
   return (
-    <BottomTab.Navigator initialRouteName="Map" tabBarOptions={{ activeTintColor: "#FD4344" }}>
+    <BottomTab.Navigator
+      initialRouteName="Map"
+      tabBarOptions={{ activeTintColor: "#FD4344" }}
+    >
       <BottomTab.Screen
         name="Map"
         component={MapNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => <MapSvg fill={color} />,
         }}
       />
       <BottomTab.Screen
         name="TabTwo"
         component={TabTwoNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ color }) => <TicketSvg fill={color} />,
         }}
       />
     </BottomTab.Navigator>
-  )
-}
-
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>["name"]; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />
+  );
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const MapStack = createStackNavigator<MapParamList>()
+const MapStack = createStackNavigator<MapParamList>();
 
 function MapNavigator() {
   return (
@@ -54,10 +52,10 @@ function MapNavigator() {
         options={{ headerTitle: "Tab One Title" }}
       />
     </MapStack.Navigator>
-  )
+  );
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>()
+const TabTwoStack = createStackNavigator<TabTwoParamList>();
 
 function TabTwoNavigator() {
   return (
@@ -68,5 +66,5 @@ function TabTwoNavigator() {
         options={{ headerTitle: "Tab Two Title" }}
       />
     </TabTwoStack.Navigator>
-  )
+  );
 }

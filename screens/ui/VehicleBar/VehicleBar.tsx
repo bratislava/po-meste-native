c62e15544ed8468660d94db49477678350156e14
Martin Pinter <martin.pinter@vacuumlabs.com>
2021-08-11 15:09:12 +0200

    Remove KeyboardAvoidingView from VehicleBar

--- screens/ui/VehicleBar/VehicleBar.tsx
+++ screens/ui/VehicleBar/VehicleBar.tsx
@@ -1,51 +1,48 @@
-import React from "react";
-import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
-import TicketSvg from "../../../assets/images/ticket.svg";
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import TicketSvg from '../../../assets/images/ticket.svg'
 
 const VehicleBar = () => {
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.vehicleBar}
-    >
+    <View style={styles.vehicleBar}>
       <View style={styles.iconLeft}>
         {/* TODO change for vehicle icon */}
-        <TicketSvg fill={"green"} />
+        <TicketSvg fill={'green'} />
       </View>
       <View>
         {/* TODO change for vehicle icon */}
-        <TicketSvg fill={"green"} />
+        <TicketSvg fill={'green'} />
       </View>
       <View>
         {/* TODO change for vehicle icon */}
-        <TicketSvg fill={"green"} />
+        <TicketSvg fill={'green'} />
       </View>
       <View style={styles.iconRight}>
         {/* TODO change for vehicle icon */}
-        <TicketSvg fill={"green"} />
+        <TicketSvg fill={'green'} />
       </View>
-    </KeyboardAvoidingView>
-  );
-};
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   vehicleBar: {
-    display: "flex",
+    display: 'flex',
     flex: 1,
-    position: "absolute",
-    bottom: "0%",
+    position: 'absolute',
+    bottom: '0%',
     marginBottom: 10,
     padding: 10,
-    width: "90%",
+    width: '90%',
     height: 50,
     minHeight: 50,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "white",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    alignItems: 'center',
     borderRadius: 15,
   },
   iconLeft: {
@@ -54,6 +51,6 @@ const styles = StyleSheet.create({
   iconRight: {
     marginRight: 20,
   },
-});
+})
 
-export default VehicleBar;
+export default VehicleBar

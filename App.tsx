037aecca6f454bb50f0c4bfae2d39079e03e3dea
Adam Grund <adam.grund@gmail.com>
2021-08-11 16:19:05 +0200

    PLAN-86 add i18n and add example text

--- App.tsx
+++ App.tsx
@@ -2,11 +2,19 @@ import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import * as Localization from 'expo-localization'
+import i18n from 'i18n-js'
+import * as translations from './translations'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import useCachedResources from './hooks/useCachedResources'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
-import { QueryClient, QueryClientProvider } from 'react-query'
+
+i18n.translations = translations
+
+i18n.locale = Localization.locale
+i18n.fallbacks = true
 
 const queryClient = new QueryClient()
 
---
+++ translations/en.js
@@ -0,0 +1,6 @@
+export default {
+  tabOneTitle: 'Tab One Title',
+  tabTwoTitle: 'Tab Two Title',
+  map: 'Map',
+  tabTwo: 'TabTwo',
+}

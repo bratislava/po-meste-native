3f69b39471adf624ff34b61244df9dbb71d7f1db
Adam Grund <adam.grund@gmail.com>
2021-08-19 17:13:44 +0200

    PLAN-93 add zse chargers to

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -10,6 +10,7 @@ import useRekolaData from '../hooks/useRekolaData'
 import useSlovnaftbajkData from '../hooks/useSlovnaftbajkData'
 import useTierData from '../hooks/useTierData'
 import useMhdData from '../hooks/useMhdData'
+import useZseChargersData from '../hooks/useZseChargersData'
 
 interface DataStations {
   station_id: string
@@ -28,6 +29,8 @@ export default function MapScreen() {
   // TODO handle loading / error
   const { data: dataMhd, isLoading: isLoadingMhd } = useMhdData()
   const { data: dataTier, isLoading: isLoadingTier } = useTierData()
+  const { data: dataZseChargers, isLoading: isLoadingZseChargers } =
+    useZseChargersData()
   const { data: dataMergedRekola, isLoading: isLoadingRekola } = useRekolaData()
   const { data: dataMergedSlovnaftbajk, isLoading: isLoadingSlovnaftbajk } =
     useSlovnaftbajkData()
@@ -96,11 +99,32 @@ export default function MapScreen() {
 
         {renderStations(dataMergedRekola, 'pink')}
         {renderStations(dataMergedSlovnaftbajk, 'yellow')}
+        {dataZseChargers?.reduce<JSX.Element[]>((accumulator, charger) => {
+          if (charger.coordinates.latitude && charger.coordinates.longitude) {
+            const marker = (
+              <Marker
+                key={charger.id}
+                coordinate={{
+                  latitude: charger.coordinates.latitude,
+                  longitude: charger.coordinates.longitude,
+                }}
+                tracksViewChanges={false}
+              >
+                <View style={styles.marker}>
+                  <TicketSvg width={30} height={40} fill="green" />
+                </View>
+              </Marker>
+            )
+            return accumulator.concat(marker)
+          } else return accumulator
+        }, [])}
       </MapView>
+
       {isLoadingMhd ||
       isLoadingRekola ||
       isLoadingSlovnaftbajk ||
-      isLoadingTier ? (
+      isLoadingTier ||
+      isLoadingZseChargers ? (
         <LoadingView />
       ) : null}
       <SearchBar />

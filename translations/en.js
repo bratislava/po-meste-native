820dff69b4f5754a8f3a15f24d47e81a8e405a69
Adam Grund <adam.grund@gmail.com>
2021-08-24 10:02:14 +0200

    PLAN-143 WIP add network request to OTP

--- translations/en.js
+++ translations/en.js
@@ -14,4 +14,8 @@ export default {
     'SMS lístok platí len na linkách MHD 1 až 212, N1 - N99, X1 - X99. \n Cestujúci by do vozidla MHD mal nastupovať až s prijatou SMS správou. \n Po kliknutí na tlačidlo sa automaticky odošle  sms spoplatnená podľa tarify uvedenej pri konkrétnom type sms lístka.',
   smsNotAvailable: 'This is unfortunate! Sms on this device is not available.',
   smsOK: 'OK',
+  whereTo: 'Where to',
+  from: 'From...',
+  to: 'To...',
+  findRoute: 'Find route',
 }
--- translations/sk.js
+++ translations/sk.js
@@ -12,4 +12,8 @@ export default {
   smsTicketTitle: 'Sms lístok',
   smsInfo:
     'SMS lístok platí len na linkách MHD 1 až 212, N1 - N99, X1 - X99. /n Cestujúci by do vozidla MHD mal nastupovať až s prijatou SMS správou. /n Po kliknutí na tlačidlo sa automaticky odošle  sms spoplatnená podľa tarify uvedenej pri konkrétnom type sms lístka.',
+  whereTo: 'Kamže kam?',
+  from: 'Odkial...',
+  to: 'Kam...',
+  findRoute: 'Nájdi trasu',
 }

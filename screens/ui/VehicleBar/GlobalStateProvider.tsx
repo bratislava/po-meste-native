90743edae6d3625af1c4a26eed4c660311410912
Adam Grund <adam.grund@gmail.com>
2021-08-26 16:25:16 +0200

    PLAN-157 add filter on vehicle types

---
+++ screens/ui/VehicleBar/GlobalStateProvider.tsx
@@ -0,0 +1,52 @@
+import React, { useState, createContext, Dispatch, SetStateAction } from 'react'
+import TicketSvg from '../../../assets/images/ticket.svg'
+import MhdSvg from '../../../assets/images/mhd.svg'
+import { VehicleType } from '../../../types'
+
+interface VehicleFilterProps {
+  children: React.ReactNode
+}
+
+interface VehicleProps {
+  id: string
+  show: boolean
+  icon: (color: string) => React.ReactNode
+}
+
+interface VehicleFilterContextProps {
+  vehicleTypes: VehicleProps[]
+  setVehicleTypes: Dispatch<SetStateAction<VehicleProps[]>>
+}
+
+export const GlobalStateContext = createContext({} as VehicleFilterContextProps)
+
+export default function GlobalStateProvider({ children }: VehicleFilterProps) {
+  const [vehicleTypes, setVehicleTypes] = useState<VehicleProps[]>([
+    {
+      id: VehicleType.mhd,
+      show: true,
+      icon: (color: string) => <MhdSvg fill={color} />, //TODO change for vehicle icon
+    },
+    {
+      id: VehicleType.bicycle,
+      show: true,
+      icon: (color: string) => <TicketSvg fill={color} />, //TODO change for vehicle icon
+    },
+    {
+      id: VehicleType.scooter,
+      show: true,
+      icon: (color: string) => <TicketSvg fill={color} />, //TODO change for vehicle icon
+    },
+    {
+      id: VehicleType.chargers,
+      show: true,
+      icon: (color: string) => <TicketSvg fill={color} />, //TODO change for vehicle icon
+    },
+  ])
+
+  return (
+    <GlobalStateContext.Provider value={{ vehicleTypes, setVehicleTypes }}>
+      {children}
+    </GlobalStateContext.Provider>
+  )
+}

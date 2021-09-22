1382f132cb253106858c1af78d0752ea645ad33c
Matej Hospodar <matejhospodar123@gmail.com>
2021-09-22 16:12:20 +0200

    bottom tab navigator redesigned

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -4,15 +4,27 @@
  */
 
 import React, { useContext } from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native'
+import {
+  createBottomTabNavigator,
+  BottomTabBarProps,
+} from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
 import i18n from 'i18n-js'
 
-import MapSvg from '../assets/images/map.svg'
-import TicketSvg from '../assets/images/ticket.svg'
+import HomeSearchSvg from '../assets/images/home-search.svg'
+import TicketSvg from '../assets/images/ticket-alt.svg'
+import BurgerMenuSvg from '../assets/images/burger-menu.svg'
+
+import TicketsScreen from '../screens/TicketsScreen'
 import MapScreen from '../screens/MapScreen'
 import TabTwoScreen from '../screens/TabTwoScreen'
-import { BottomTabParamList, MapParamList, TabTwoParamList } from '../types'
+import {
+  BottomTabParamList,
+  TicketsParamList,
+  MapParamList,
+  TabTwoParamList,
+} from '../types'
 import SmsScreen from '../screens/SmsScreen'
 import FromToScreen from '../screens/FromToScreen'
 import PlannerScreen from '../screens/PlannerScreen'
@@ -27,32 +39,160 @@ export default function BottomTabNavigator() {
   return (
     <BottomTab.Navigator
       initialRouteName="Map"
-      tabBarOptions={{ activeTintColor: '#FD4344' }}
+      tabBarOptions={{
+        activeTintColor: '#FD4344',
+      }}
+      tabBar={(props) => <TabBar {...props} />}
     >
+      <BottomTab.Screen
+        name="Tickets"
+        component={TicketsNavigator}
+        options={{ title: i18n.t('tickets'), tabBarIcon: TicketSvg }}
+      />
       <BottomTab.Screen
         name="Map"
         component={MapNavigator}
-        options={{
-          title: i18n.t('map'),
-          // eslint-disable-next-line react/prop-types
-          tabBarIcon: ({ color }) => <MapSvg fill={color} />,
-        }}
+        options={{ title: i18n.t('searching'), tabBarIcon: HomeSearchSvg }}
       />
       <BottomTab.Screen
         name="TabTwo"
         component={TabTwoNavigator}
-        options={{
-          title: i18n.t('tabTwo'),
-          // eslint-disable-next-line react/prop-types
-          tabBarIcon: ({ color }) => <TicketSvg fill={color} />,
-        }}
+        options={{ title: i18n.t('settings'), tabBarIcon: BurgerMenuSvg }}
       />
     </BottomTab.Navigator>
   )
 }
 
+const shadow = {
+  shadowColor: '#000',
+  shadowRadius: 5,
+  shadowOpacity: 0.2,
+}
+
+const styles = StyleSheet.create({
+  tabBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 55,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    ...shadow,
+  },
+  tabWrapper: {
+    width: 80,
+    height: 80,
+    position: 'relative',
+  },
+  tabShadow: {
+    width: 80,
+    height: 80,
+    position: 'absolute',
+    top: 1,
+    backgroundColor: '#fff',
+    borderRadius: 40,
+    ...shadow,
+  },
+  tabBackground: {
+    width: 95,
+    height: 80,
+    position: 'absolute',
+    top: 12.5,
+    left: -10,
+    backgroundColor: '#fff',
+  },
+  tab: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 2,
+    width: 80,
+    height: 80,
+  },
+})
+
+const TabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
+  return (
+    <View style={styles.tabBar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key]
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name
+
+        const isFocused = state.index === index
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          })
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name)
+          }
+        }
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          })
+        }
+
+        return (
+          <TouchableWithoutFeedback
+            key={index}
+            onPress={onPress}
+            onLongPress={onLongPress}
+          >
+            <View style={styles.tabWrapper}>
+              <View
+                style={{
+                  ...styles.tabShadow,
+                  opacity: isFocused ? 1 : 0,
+                }}
+              />
+              <View style={styles.tabBackground} />
+              <View style={styles.tab}>
+                <options.tabBarIcon fill={isFocused ? '#FD4344' : 'gray'} />
+                <Text>{label}</Text>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        )
+      })}
+    </View>
+  )
+}
+
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+const TicketsStack = createStackNavigator<TicketsParamList>()
+
+function TicketsNavigator() {
+  return (
+    <TicketsStack.Navigator screenOptions={{}}>
+      <TicketsStack.Screen
+        name="TicketsScreen"
+        component={TicketsScreen}
+        options={{ headerTitle: i18n.t('tabOneTitle'), headerShown: false }}
+      />
+    </TicketsStack.Navigator>
+  )
+}
+
 const MapStack = createStackNavigator<MapParamList>()
 
 function MapNavigator() {

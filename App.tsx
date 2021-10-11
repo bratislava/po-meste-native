5e2b048df5a5b6486bbc69ee7aba33e6a91190af
Matej Hospodar <matejhospodar123@gmail.com>
2021-10-11 17:16:37 +0200

    Global state provider united

--- App.tsx
+++ App.tsx
@@ -13,7 +13,6 @@ import * as Sentry from 'sentry-expo'
 import useCachedResources from '@hooks/useCachedResources'
 import useColorScheme from '@hooks/useColorScheme'
 import Navigation from '@navigation/index'
-import VehicleGlobalStateProvider from '@screens/ui/VehicleBar/GlobalStateProvider'
 import GlobalStateProvider from '@components/GlobalStateProvider'
 
 i18n.translations = translations
@@ -40,14 +39,12 @@ export default function App() {
   } else {
     return (
       <GlobalStateProvider>
-        <VehicleGlobalStateProvider>
-          <QueryClientProvider client={queryClient}>
-            <SafeAreaProvider>
-              <Navigation colorScheme={colorScheme} />
-              <StatusBar />
-            </SafeAreaProvider>
-          </QueryClientProvider>
-        </VehicleGlobalStateProvider>
+        <QueryClientProvider client={queryClient}>
+          <SafeAreaProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </SafeAreaProvider>
+        </QueryClientProvider>
       </GlobalStateProvider>
     )
   }
--- components/GlobalStateProvider.tsx
+++ components/GlobalStateProvider.tsx
@@ -1,22 +1,64 @@
 import React, { useState, createContext, Dispatch, SetStateAction } from 'react'
+import { VehicleType } from 'types'
+import MhdSvg from '@images/mhd.svg'
+import TicketSvg from '@images/ticket.svg'
 
 interface Props {
   children: React.ReactNode
 }
 
 interface ContextProps {
+  vehicleTypes: VehicleProps[]
+  setVehicleTypes: Dispatch<SetStateAction<VehicleProps[]>>
+  timeLineNumber?: number
+  setTimeLineNumber: React.Dispatch<React.SetStateAction<number | undefined>>
   isFeedbackSent: boolean
   setFeedbackSent: Dispatch<SetStateAction<boolean>>
+}
+
+interface VehicleProps {
+  id: string
+  show: boolean
+  icon: (color: string) => React.ReactNode
 }
 
 export const GlobalStateContext = createContext({} as ContextProps)
 
 export default function GlobalStateProvider({ children }: Props) {
+  const [timeLineNumber, setTimeLineNumber] = useState<number>()
+
+  const [vehicleTypes, setVehicleTypes] = useState<VehicleProps[]>([
+    {
+      id: VehicleType.mhd,
+      show: true,
+      icon: (color: string) => <MhdSvg fill={color} />, //TODO change for vehicle icon
+    },
+    {
+      id: VehicleType.bicycle,
+      show: true,
+      icon: (color: string) => <TicketSvg fill={color} />, //TODO change for vehicle icon
+    },
+    {
+      id: VehicleType.scooter,
+      show: true,
+      icon: (color: string) => <TicketSvg fill={color} />, //TODO change for vehicle icon
+    },
+    {
+      id: VehicleType.chargers,
+      show: true,
+      icon: (color: string) => <TicketSvg fill={color} />, //TODO change for vehicle icon
+    },
+  ])
+
   const [isFeedbackSent, setFeedbackSent] = useState(false)
 
   return (
     <GlobalStateContext.Provider
       value={{
+        vehicleTypes,
+        setVehicleTypes,
+        timeLineNumber,
+        setTimeLineNumber,
         isFeedbackSent,
         setFeedbackSent,
       }}

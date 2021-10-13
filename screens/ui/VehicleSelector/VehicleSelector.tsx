c64966a5a71c85c8d1fdaba4e02a8828cb87bef7
Matej Hospodar <matejhospodar123@gmail.com>
2021-10-13 14:01:43 +0200

    vehicle selector implemented

---
+++ screens/ui/VehicleSelector/VehicleSelector.tsx
@@ -0,0 +1,122 @@
+import React, { ReactSVGElement } from 'react'
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native'
+import { SvgProps } from 'react-native-svg'
+
+import { Modes } from '../../../types'
+import { colors } from '@utils/theme'
+
+export type VehicleType = {
+  mode: Modes
+  icon: React.FC<SvgProps>
+  estimatedTime: string
+  price: string
+}
+
+type Props = {
+  selectedVehicle: Modes
+  vehicles: VehicleType[]
+  onVehicleChange: (mode: Modes) => void
+}
+
+const VehicleSelector = ({
+  selectedVehicle,
+  onVehicleChange,
+  vehicles,
+}: Props) => {
+  return (
+    <ScrollView horizontal>
+      <View style={styles.container}>
+        {vehicles.map((vehicle, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={[
+                styles.vehicleContainer,
+                selectedVehicle == vehicle.mode
+                  ? styles.vehicleContainerSelected
+                  : {},
+                index + 1 != vehicles.length ? styles.vehicleContainerLast : {},
+              ]}
+              onPress={() => onVehicleChange(vehicle.mode)}
+            >
+              <vehicle.icon
+                fill={selectedVehicle == vehicle.mode ? 'white' : colors.gray}
+                width={24}
+                height={20}
+                style={styles.vehicleIcon}
+              />
+              <Text
+                style={[
+                  styles.vehicleEstimatedTime,
+                  selectedVehicle == vehicle.mode
+                    ? styles.vehicleEstimatedTimeSelected
+                    : {},
+                ]}
+              >
+                {vehicle.estimatedTime}
+              </Text>
+              <Text
+                style={[
+                  styles.vehiclePrice,
+                  selectedVehicle == vehicle.mode
+                    ? styles.vehiclePriceSelected
+                    : {},
+                ]}
+              >
+                {vehicle.price}
+              </Text>
+            </TouchableOpacity>
+          )
+        })}
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+  },
+  vehicleContainer: {
+    borderWidth: 1,
+    borderLeftColor: colors.gray,
+    borderRadius: 3,
+    borderColor: colors.gray,
+    width: 85,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  vehicleContainerLast: {
+    marginRight: 10,
+  },
+  vehicleContainerSelected: {
+    borderWidth: 0,
+    backgroundColor: colors.primary,
+  },
+  vehicleIcon: {
+    marginBottom: 5,
+  },
+  vehicleEstimatedTime: {
+    fontWeight: 'bold',
+    color: colors.gray,
+  },
+  vehicleEstimatedTimeSelected: {
+    color: 'white',
+  },
+  vehiclePrice: {
+    color: colors.gray,
+  },
+  vehiclePriceSelected: {
+    color: 'white',
+  },
+})
+
+export default VehicleSelector

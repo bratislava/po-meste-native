29a4803c764b99b050c21225a02577928f034f71
Balros <adam.grund@gmail.com>
2021-10-13 15:15:35 +0200

    add release config data
    * add release config data
    
    * release-channels add additional info to release application
    
    * shadow  tuned
    
    * from to selector component added
    
    * FromToSelector component added, header removed and restyled
    
    * icons added
    
    * header removed from FromToScreen
    
    * colors object removed
    
    * feature/PLAN-183 add from/to screen using BottomSheet 
    
    * WIP PLAN-183 add search From To screen
    
    * WIP PLAN-183 add search From To screen
    
    * SearchFromToScreen styling
    
    * shadow  tuned
    
    * from to selector component added
    
    * FromToSelector component added, header removed and restyled
    
    * icons added
    
    * header removed from FromToScreen
    
    * colors object removed
    
    * WIP PLAN-183 add search From To screen
    
    * SearchFromToScreen styling
    
    * WIP PLAN-183 add search From To screen
    
    * PLAN-183 hook FromToScreen on FromToSelector
    
    * PLAN-183 add suggestion for time manipulation
    
    * SearchFromToScreen styling
    
    * WIP PLAN-183 add search From To screen
    
    Co-authored-by: Matej Hospodar <matejhospodar123@gmail.com>
    
    * Update .env.example
    
    Co-authored-by: Martin Pinter <mpinter@users.noreply.github.com>
    
    * add release config data
    
    * release-channels add additional info to release application
    
    * Update .env.example
    
    Co-authored-by: Martin Pinter <mpinter@users.noreply.github.com>
    
    Co-authored-by: Matej Hospodar <matejhospodar123@gmail.com>
    Co-authored-by: Martin Pinter <mpinter@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import * as Localization from 'expo-localization'
 import i18n from 'i18n-js'
 import * as translations from './translations'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import * as Sentry from 'sentry-expo'
 
 import useCachedResources from '@hooks/useCachedResources'
 import useColorScheme from '@hooks/useColorScheme'
@@ -18,6 +19,12 @@ i18n.translations = translations
 
 i18n.locale = Localization.locale
 i18n.fallbacks = true
+
+Sentry.init({
+  dsn: 'https://939d2a9ea74b4121be8ba1e2e984a0b1@o701870.ingest.sentry.io/5993278', //found in Settings > Client Keys tab
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+})
 
 const queryClient = new QueryClient()
 

596da762f07ec4f0c04908992087ca6b753c626e
Adam Grund <adam.grund@gmail.com>
2021-12-06 16:45:08 +0100

    release 1.0.9

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   name: 'hybaj',
   owner: 'bratislava',
   slug: 'hybaj',
-  version: '1.0.8',
+  version: '1.0.9',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',

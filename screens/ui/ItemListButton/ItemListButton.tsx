3a1acf626da5ca9be4b1bb6018b8c191ff36521e
Matej Hospodar <matejhospodar123@gmail.com>
2021-12-07 17:46:38 +0100

    add item list button component

---
+++ screens/ui/ItemListButton/ItemListButton.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import ChevronRightSmall from '@images/chevron-right-small.svg'
+import { colors } from '@utils/theme'
+import { SvgProps } from 'react-native-svg'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+export interface ItemListButtonProps {
+  icon: FC<SvgProps>
+  text: string
+  onPress: () => void
+}
+
+export const ItemListButton = ({
+  icon: Icon,
+  text,
+  onPress,
+}: ItemListButtonProps) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <View style={styles.leftContainer}>
+          <Icon width={32} height={32} fill={colors.primary} />
+          <Text style={styles.text}>{text}</Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <ChevronRightSmall width={16} height={16} fill={colors.gray} />
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    marginLeft: 20,
+    fontWeight: 'bold',
+  },
+  rightContainer: {},
+})
+
+export default ItemListButton

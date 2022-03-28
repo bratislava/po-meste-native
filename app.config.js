8a4da5293f0a22828aba46d93fc8c5c452dc6a0e
Balros <adam.grund@gmail.com>
2022-03-28 13:45:38 +0200

    Feature/plan 375 (#102)
    * remove one additional minute to vehicle arrival time in UpcomingDepartures
    
    * readme fix + bump up version
    
    * bump up version + versionCode + buildNumber

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   name: 'Po Meste',
   owner: 'bratislava',
   slug: 'hybaj',
-  version: '1.1.5',
+  version: '1.2.0',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',
@@ -21,7 +21,7 @@ export default {
   ios: {
     bundleIdentifier: 'com.bratislava.hybaj',
     supportsTablet: false,
-    buildNumber: '23',
+    buildNumber: '30',
     infoPlist: {
       NSLocationWhenInUseUsageDescription:
         'Allow the app to use your location to show it on map and suggest navigation.',
@@ -37,7 +37,7 @@ export default {
   android: {
     softwareKeyboardLayoutMode: 'pan',
     package: 'com.bratislava.hybaj',
-    versionCode: 23,
+    versionCode: 30,
     adaptiveIcon: {
       foregroundImage: './assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',

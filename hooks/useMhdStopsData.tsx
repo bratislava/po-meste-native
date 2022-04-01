cdc2fd4ca0ca70fb798857cc5929d6cdcd723851
lukybil <lukybil@gmail.com>
2022-04-01 17:56:09 +0200

    Tried to implement caching health check but encountered weird error

--- hooks/useMhdStopsData.tsx
+++ hooks/useMhdStopsData.tsx
@@ -2,15 +2,34 @@ import { useMemo, useState } from 'react'
 import { useQuery } from 'react-query'
 
 import { apiMhdStops } from '../utils/validation'
-import { getMhdStops } from '../utils/api'
-import { getCachedStops } from '@utils/utils'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { getHealth, getMhdStops } from '../utils/api'
+import {
+  getCachedStops,
+  getLatestDataset,
+  setCachedStops,
+  setLatestDataset,
+} from '@utils/utils'
 
 export default function useMhdData() {
   const [validationErrors, setValidationErrors] = useState()
+  const { data: healthData, error: healthError } = useQuery(
+    'getHealth',
+    getHealth
+  )
+  const { data: latestDatasetData } = useQuery(
+    'getLastDataSet',
+    getLatestDataset
+  )
+  const fetchNewData = useMemo(() => {
+    if (healthError != undefined) return true
+    if (healthData == undefined) return false
+    if (healthData.latestDataset === latestDatasetData) return false
+    else return true
+  }, [healthData, healthError, latestDatasetData])
   const { data, isLoading, error, refetch } = useQuery(
     'getMhdStops',
-    getMhdStops
+    getMhdStops,
+    { enabled: fetchNewData }
   )
   const { data: cachedData } = useQuery('getCachedMhdStops', () =>
     getCachedStops('mhdStops')
@@ -18,15 +37,17 @@ export default function useMhdData() {
 
   const validatedMhdStops = useMemo(() => {
     if (data == null) return cachedData
-    try {
-      const mhdStops = apiMhdStops.validateSync(data)
-      AsyncStorage.setItem('mhdStops', JSON.stringify(mhdStops))
-      return mhdStops
-    } catch (e: any) {
-      setValidationErrors(e.errors)
-      console.log(e)
-    }
-  }, [data, cachedData])
+    if (fetchNewData)
+      try {
+        const mhdStops = apiMhdStops.validateSync(data)
+        setCachedStops('mhdStops', mhdStops)
+        setLatestDataset(healthData.latestDataset)
+        return mhdStops
+      } catch (e: any) {
+        setValidationErrors(e.errors)
+        console.log(e)
+      }
+  }, [data, cachedData, fetchNewData])
 
   return {
     data: validatedMhdStops,

cb512029a43a654c47a3e99ccda3ce836b467bb4
lukybil <lukybil@gmail.com>
2022-08-08 16:45:23 +0200

    changed spinner colors

--- components/VehicleBar.tsx
+++ components/VehicleBar.tsx
@@ -110,7 +110,7 @@ const VehicleFilterTouchable = ({
       <View>
         {isLoading && (
           <Progress.CircleSnail
-            color={colors.secondary}
+            color={index === 0 ? colors.secondary : colors.primary}
             size={ICON_SIZE + 6}
             borderWidth={0}
             spinDuration={2000}

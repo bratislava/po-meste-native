d2f77fb7a600cc63358cfe8fd6f0455d7cda08f7
lukybil <lukybil@gmail.com>
2022-11-23 16:06:47 +0100

    update sentry

--- App.tsx
+++ App.tsx
@@ -36,7 +36,7 @@ i18n.pluralization['sk'] = function (count) {
 Sentry.init({
   dsn: 'https://939d2a9ea74b4121be8ba1e2e984a0b1@o701870.ingest.sentry.io/5993278', //found in Settings > Client Keys tab
   enableInExpoDevelopment: true,
-  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+  debug: __DEV__ ? true : false, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
 })
 
 LogBox.ignoreLogs(['Setting a timer']) // https://github.com/tannerlinsley/react-query/issues/1259#issuecomment-799630607
